94468161c5ad3a02a6c65cc930cf5233097d2bba
nem4tgf <143236099+nem4tgf@users.noreply.github.com>
2024-03-24 22:28:24 +0700

    Update Person.java

--- src/session3/Person.java
+++ src/session3/Person.java
@@ -1,4 +1,34 @@
 package session3;
 
-public class Person {
+public class Person  extends Animal{
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getAnimalName() {
+        return "Hung";
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
+
